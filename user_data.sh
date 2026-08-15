1eee9cf5fc3c8b273af1eaf8a53d820f0cdb647b
mepranavpatil <mepranavpatil@gmail.com>
2026-08-15 18:09:36 +0530

    Initial commit

---
+++ user_data.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+yum update -y
+yum install docker -y
+yum install git -y
+yum install nginx -y
+systemctl start nginx
+systemctl enable nginx
+systemctl start docker
+systemctl enable docker
+
+sudo su 
+docker login -u pranavpatil2704
