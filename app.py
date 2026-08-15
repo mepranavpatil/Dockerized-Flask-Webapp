f0f015b8db503e3c5c8f0c685aef5b8649e9b155
mepranavpatil <mepranavpatil@gmail.com>
2026-08-15 18:42:48 +0530

    added functions to the webapp

--- app.py
+++ app.py
@@ -6,7 +6,15 @@ app = Flask(__name__)
 def home():
     return """
     <h1>Dockerized Flask WebApp</h1>
-    <p>Welcome to Project 2</p>
+    <p>Cloud Engineer Calculator API</p>
+    """
+
+@app.route("/about")
+def about():
+    return """
+    <h1>About</h1>
+    <p>This is a simple Flask web application running inside a Docker container.</p>
+    <h4>Project is created by Pranav Patil for practice only </h4>
     """
 
 @app.route("/health")
@@ -19,6 +27,40 @@ def health():
 def version():
     return jsonify({
         "version": "1.0"
+    })
+
+@app.route("/add/<int:a>/<int:b>")
+def add(a, b):
+    return jsonify({
+        "operation": "addition",
+        "result": a + b
+    })
+
+@app.route("/sub/<int:a>/<int:b>")
+def subtract(a, b):
+    return jsonify({
+        "operation": "subtraction",
+        "result": a - b
+    })
+
+@app.route("/mul/<int:a>/<int:b>")
+def multiply(a, b):
+    return jsonify({
+        "operation": "multiplication",
+        "result": a * b
+    })
+
+@app.route("/div/<int:a>/<int:b>")
+def divide(a, b):
+
+    if b == 0:
+        return jsonify({
+            "error": "division by zero not allowed"
+        }), 400
+
+    return jsonify({
+        "operation": "division",
+        "result": a / b
     })
 
 if __name__ == "__main__":
