5618013dafaf208c469c6b0530b0ba533cf3d4c3
mepranavpatil <mepranavpatil@gmail.com>
2026-08-15 18:29:06 +0530

    removed tf files

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket       = "pranav001-backup-2704"
-    key          = "dev/terraform.tfstate"
-    region       = "ap-south-1"
-    encrypt      = true
-    use_lockfile = true
-  }
-}
